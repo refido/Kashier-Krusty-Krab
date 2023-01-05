c8ae13fa3cd57143e9b8f696806085f72bad9b1f
sellauno <sellauno03@gmail.com>
2023-01-05 23:43:29 +0700

    login background

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,34 @@
 import styled from "styled-components";
 import logoKrustyKrab from "../logo/logoKrustyKrab-1.png"
+import picture from "../logo/Squidward&Patrick.png";
 
 function Login() {
-    return(
-        <LoginLayout> 
-             <Logo src={logoKrustyKrab} />
+    return (
+        <LoginLayout>
+            <Container>
+                <Logo src={logoKrustyKrab} />
+                <div>
+                    <img src={picture} style={{ margin:40, width: 400, height:400 }}></img>
+
+                </div>
+            </Container>
         </LoginLayout>
     );
 }
 
 const LoginLayout = styled.div`
-    height: 100vh;
-    display: flex;
+    height: max-width;
     background: linear-gradient(180deg, #83EAF3 0%, #77E1EF 26.95%, #40AEDF 67.7%, #2EA0D7 100%);
 `;
 
 const Logo = styled.img`
-    margin: 20;
-    width: 50; 
-    height: 50;
+    margin: 30px;
+    width: 74px; 
+    height: 71px;
+`;
+
+const Container = styled.div`
+    background-image: url('/background-bunga.png');
 `;
 
 export default Login;
