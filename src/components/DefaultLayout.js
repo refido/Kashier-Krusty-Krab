aa88eb00cea5f9352d6eaf6d50a4a5e2f1cbe3b5
revido <revidoberliano@gmail.com>
2023-01-11 19:35:34 +0700

    listorder component

--- src/components/DefaultLayout.js
+++ src/components/DefaultLayout.js
@@ -1,23 +1,30 @@
-import { Layout } from 'antd'
+import { Col, Layout, Row } from 'antd'
 import { Content } from 'antd/es/layout/layout'
 import React from 'react'
 import "../styles/KashierItemComponent.css"
-
+import "../styles/ListOrder.css"
+import ListOrder from "./ListOrder";
 function DefaultLayout({ children }) {
   return (
     <Layout>
-      <Layout className='site-layout'>
-        <Content
-          className='site-layout-background'
-          style={{
-            margin: "24px 16px",
-            padding: 24,
-            minHeight: 280,
-          }}
-        >
-          {children}
-        </Content>
-      </Layout>
+      <Row>
+        <ListOrder />
+        <Col span={16} pull={8}>
+          <Layout className='site-layout'>
+            <Content
+              className='site-layout-background'
+              style={{
+                margin: "24px 16px",
+                left: 0,
+                top: 0,
+              }}
+            >
+              Choose Category
+              {children}
+            </Content>
+          </Layout>
+        </Col>
+      </Row>
     </Layout>
   )
 }
